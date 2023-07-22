5f9c95f4997d414357d2953a5c8be31d12bc9cc2
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-07-22 21:33:11 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 import axios from 'axios';
 import { creatGalleryMarkup } from './markup';
-
+Notify.init({
+  opacity: 0.5,
+  timeout: 1000,
+});
 const URL = 'https://pixabay.com/api/';
 const key = '38388037-1512b522da0d657b891be2adb';
 const form = document.querySelector('.search-form');
@@ -12,17 +15,29 @@ const gallery = document.querySelector('.gallery');
 const target = document.querySelector('.js-guard');
 const btnUp = document.querySelector('.button-up');
 const btnDown = document.querySelector('.button-down');
+const arrowBtns = document.querySelector('.arrow-buttons');
+let options = {
+  root: null,
+  rootMargin: '300px',
+  threshold: 0,
+};
+let observer = new IntersectionObserver(observerHandler, options);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
 
 let page = 1;
 let search = '';
 let totalHits = 0;
-let lightbox;
+
 async function getPictures(page, value) {
   const response = await axios(
     `${URL}?key=${key}&q=${value}&image_type=photo$orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
   return response;
 }
+
 form.addEventListener('submit', onClick);
 
 async function onClick(e) {
@@ -31,7 +46,7 @@ async function onClick(e) {
     elements: { searchQuery },
   } = e.currentTarget;
   gallery.innerHTML = '';
-  page = 11;
+  page = 1;
   search = searchQuery.value
     .split(' ')
     .filter(item => item !== '')
@@ -39,35 +54,29 @@ async function onClick(e) {
   if (search) {
     try {
       let data = await getPictures(page, search);
-      if (data.data.total === 0) {
+      if (data.data.total === 0 || data.data.hits.length === 0) {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
+        btnDown.hidden = true;
       } else {
         gallery.insertAdjacentHTML(
           'beforeend',
           data.data.hits.map(object => creatGalleryMarkup(object)).join('')
         );
-          lightbox = new SimpleLightbox('.gallery a', {
-          captionDelay: 250,
-          captionsData: 'alt',
-        });
         Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
         totalHits = data.data.totalHits;
         observer.observe(target);
         console.log(gallery.getBoundingClientRect());
+        lightbox.refresh();
+        btnDown.hidden = false;
+        console.log(data);
       }
-    } catch (error) {}
+    } catch (error) {
+      Notify.failure('Ooops, something went wrong!');
+    }
   }
 }
-
-let options = {
-  root: null,
-  rootMargin: '300px',
-  threshold: 0,
-};
-
-let observer = new IntersectionObserver(observerHandler, options);
 
 function observerHandler(entries, observer) {
   entries.forEach(async element => {
@@ -82,29 +91,34 @@ function observerHandler(entries, observer) {
               .map(object => creatGalleryMarkup(object))
               .join('')
           );
-          lightbox.destroy();
-          lightbox = new SimpleLightbox('.gallery a', {
-            captionDelay: 250,
-            captionsData: 'alt',
-          });
-          btnUp.hidden = false;
+          lightbox.refresh();
         } catch (error) {
           Notify.failure('Ooops, something went wrong!');
         }
       }
     }
   });
-};
+}
 
-btnUp.addEventListener('click', (e) => {
-  console.log(gallery.getBoundingClientRect());
-  window.scrollBy({
-  top: gallery.getBoundingClientRect().top,
-  behavior: "smooth",
+arrowBtns.addEventListener('click', e => {
+  if (e.target === btnUp) {
+    console.log(gallery.getBoundingClientRect());
+    window.scrollBy({
+      top: gallery.getBoundingClientRect().top - 100,
+      behavior: 'smooth',
+    });
+  } else if (e.target === btnDown) {
+    window.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth',
+    });
+  }
 });
-})
 
- 
-
-
-
+document.addEventListener('scroll', skrollHandler);
+function skrollHandler(e) {
+  if (gallery.getBoundingClientRect().top < -200) {
+    btnUp.hidden = false;
+  } else btnUp.hidden = true;
+}
+console.log(window.innerHeight);
